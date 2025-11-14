63e84b78bee57c18d2e91c45a61adcacdfc35238
Damir.Davletov <sasharomanov44@mail.ru>
2025-11-14 18:22:45 +0300

    released all packages

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,25 @@
 package actioninfo
 
+import "log"
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	for _, line := range dataset {
+		if err := dp.Parse(line); err != nil {
+			log.Printf("ошибка парсинга: %v", err)
+			continue
+		}
+
+		info, err := dp.ActionInfo()
+		if err != nil {
+			log.Printf("ошибка формирования информации: %v", err)
+			continue
+		}
+
+		print(info)
+	}
 }
--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,54 @@
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
 	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
-func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+// Parse разбирает строку вида "3456,3h00m".
+func (ds *DaySteps) Parse(datastring string) error {
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 2 {
+		return errors.New("неверный формат строки")
+	}
+
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil || steps <= 0 {
+		return errors.New("количество шагов должно быть положительным числом")
+	}
+	ds.Steps = steps
+
+	dur, err := time.ParseDuration(parts[1])
+	if err != nil || dur <= 0 {
+		return errors.New("продолжительность должна быть положительной")
+	}
+	ds.Duration = dur
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	if ds.Steps <= 0 || ds.Duration <= 0 || ds.Weight <= 0 || ds.Height <= 0 {
+		return "", errors.New("неверные входные данные")
+	}
+
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+	calories, _ := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+
+	return fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+		ds.Steps, distance, calories,
+	), nil
 }
